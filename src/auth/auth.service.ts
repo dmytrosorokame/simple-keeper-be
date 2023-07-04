1f5e15207317a4e11e1f4c9ea4bac930c24059f4
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-07-04 22:05:53 +0300

    feat: complete signUp endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,33 @@
 import { PrismaService } from './../prisma.service';
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signUp({ email, password }) {
-    const user = await this.prisma.user.create({
-      data: {
+    const user = await this.prisma.user.findUnique({
+      where: {
         email,
-        password,
       },
     });
 
-    return user;
+    if (user) {
+      throw new ConflictException('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const createdUser = await this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    delete createdUser.password;
+
+    return createdUser;
   }
 }
