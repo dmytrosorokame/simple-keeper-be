8c17f1bc0bac8620ff39bbf83584e73df337ac73
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-07-04 22:57:48 +0300

    feat: create expense module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { CategoryModule } from './category/category.module';
+import { ExpenseModule } from './expense/expense.module';
 
 @Module({
-  imports: [AuthModule, CategoryModule],
+  imports: [AuthModule, CategoryModule, ExpenseModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
