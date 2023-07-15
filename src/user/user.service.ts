21c09452c5aee4d5000fb3132ec0ecb2a7f4d701
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-07-15 11:42:43 +0300

    feat: created user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+
+import { PrismaService } from './../prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prismaService: PrismaService) {}
+
+  findByEmail(email: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  findById(id: number): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  create(email: string, password: string): Promise<User> {
+    return this.prismaService.user.create({
+      data: {
+        email,
+        password,
+      },
+    });
+  }
+
+  update(id: number, data: Partial<User>): Promise<User> {
+    return this.prismaService.user.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  }
+}
