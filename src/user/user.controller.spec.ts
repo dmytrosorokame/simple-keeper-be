df3ed6dbf2d7fe26ebd27b81ed8f6e02fc548740
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-08-04 22:42:44 +0300

    feat: write files for user module

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,19 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            findById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getMe', () => {
+    it('should return the user without password and refreshToken', async () => {
+      const mockUserId = 1;
+      const mockUser = {
+        id: mockUserId,
+        email: 'test@example.com',
+        password: 'hashed_password',
+        refreshToken: 'some_refresh_token',
+      };
+
+      jest.spyOn(userService, 'findById').mockResolvedValue(mockUser);
+
+      // Mock the request object
+      const mockRequest = {
+        user: { sub: mockUserId },
+      };
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const result = await controller.getMe(mockRequest as any);
+
+      expect(result.id).toEqual(mockUser.id);
+      expect(result.email).toEqual(mockUser.email);
+      expect('password' in result).toBeFalsy();
+      expect('refreshToken' in result).toBeFalsy();
+      expect(userService.findById).toHaveBeenCalledWith(mockUserId);
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,19 +1,142 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { User } from '@prisma/client';
 
+import { PrismaService } from './../prisma.service';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: {
+            user: {
+              findUnique: jest.fn(),
+              create: jest.fn(),
+              update: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = moduleRef.get<UserService>(UserService);
+    prismaService = moduleRef.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findByEmail', () => {
+    it('should return user when a user with the given email exists', async () => {
+      const mockUser: User = {
+        id: 1,
+        email: 'test@example.com',
+        password: 'hashed_password',
+        refreshToken: null,
+      };
+
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(mockUser);
+
+      const email = 'test@example.com';
+      const result = await userService.findByEmail(email);
+
+      expect(result).toEqual(mockUser);
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { email },
+      });
+    });
+
+    it('should return null when a user with the given email does not exist', async () => {
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+
+      const email = 'nonexistent@example.com';
+      const result = await userService.findByEmail(email);
+
+      expect(result).toBeNull();
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { email },
+      });
+    });
+  });
+
+  describe('findById', () => {
+    it('should return user when a user with the given ID exists', async () => {
+      const mockUser: User = {
+        id: 1,
+        email: 'test@example.com',
+        password: 'hashed_password',
+        refreshToken: null,
+      };
+
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(mockUser);
+
+      const id = 1;
+      const result = await userService.findById(id);
+
+      expect(result).toEqual(mockUser);
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { id },
+      });
+    });
+
+    it('should return null when a user with the given ID does not exist', async () => {
+      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+
+      const id = 100;
+      const result = await userService.findById(id);
+
+      expect(result).toBeNull();
+      expect(prismaService.user.findUnique).toHaveBeenCalledWith({
+        where: { id },
+      });
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const mockUser: User = {
+        id: 1,
+        email: 'test@example.com',
+        password: 'hashed_password',
+        refreshToken: null,
+      };
+
+      jest.spyOn(prismaService.user, 'create').mockResolvedValue(mockUser);
+
+      const email = 'test@example.com';
+      const password = 'password123';
+      const result = await userService.create(email, password);
+
+      expect(result).toEqual(mockUser);
+      expect(prismaService.user.create).toHaveBeenCalledWith({
+        data: { email, password },
+      });
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user with the given ID and data', async () => {
+      const mockUser: User = {
+        id: 1,
+        email: 'test@example.com',
+        password: 'hashed_password',
+        refreshToken: null,
+      };
+
+      jest.spyOn(prismaService.user, 'update').mockResolvedValue(mockUser);
+
+      const id = 1;
+      const updatedData = { email: 'updated@example.com' };
+      const result = await userService.update(id, updatedData);
+
+      expect(result).toEqual(mockUser);
+      expect(prismaService.user.update).toHaveBeenCalledWith({
+        where: { id },
+        data: updatedData,
+      });
+    });
   });
 });
